e3d75ab2db178c5097a660647d5cb6ed90a3d275
ghufran919 <ahmadghufranakbar@gmail.com>
2022-08-07 17:34:41 +0800

    Menambahkan js

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("calculate");
+
+btn.addEventListener("click", function () {
+  let height = document.querySelector("#height").value;
+  let weight = document.querySelector("#weight").value;
+
+  preventDefault()
+
+
+});
