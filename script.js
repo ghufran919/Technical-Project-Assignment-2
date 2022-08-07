346fc849dd1d8a81188b13c8363dab445b34ef26
ghufran919 <ahmadghufranakbar@gmail.com>
2022-08-07 18:14:09 +0800

    Event Alert

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const btn = document.getElementById("calculate");
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function (e) { e.preventDefault(); 
   let height = document.querySelector("#height").value;
   let weight = document.querySelector("#weight").value;
+  
+  if((height == '') || (height < 0) || (isNaN(height))){
+    //NaN !== NaN
+    alert("Tolong masukkan angka yang sesuai!");
+    return;
+  }
+    
+ else if (weight == '' || weight < 0 || isNaN(weight)){
+    alert("Tolong masukkan angka yang sesuai!");
+    return;
+  }
 
-  preventDefault()
+else
+//calculate BMI
 
-
-});
+})
